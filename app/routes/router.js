2c3af283c83a48b70d509fe6ac58868e0132eed3
Arthur Santos <108244695+xDef4lt@users.noreply.github.com>
2023-03-24 13:21:02 -0300

    Update router.js

--- app/routes/router.js
+++ app/routes/router.js
@@ -1,8 +1,20 @@
-let express = require('express');
+let express = require("express");
 let router = express.Router();
 
-router.get('/',function (req, res) {
-    res.render('pages/index');
+router.get("/", function (req, res) {
+    res.render("pages/index");
+});
+router.get("/html", function (req, res) {
+    res.render("pages/html");
+});
+router.get("/jquery", function (req, res) {
+    res.render("pages/jquery");
+});
+router.post("/html", function (req, res) {
+    res.json(req.body);
+});
+router.post("/jquery", function (req, res) {
+    console.log(req.body)
 });
 
 module.exports = router;

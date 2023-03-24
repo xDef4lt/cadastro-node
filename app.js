b7b61fd58f3245d7ab88170b75fb3943aa222a27
Arthur Santos <108244695+xDef4lt@users.noreply.github.com>
2023-03-24 13:20:30 -0300

    Update app.js

--- app.js
+++ app.js
@@ -7,6 +7,9 @@ app.use(express.static('app/public'));
 app.set('view engine', 'ejs');
 app.set('views', './app/views');
 
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
 const rotas = require('./app/routes/router');
 app.use('/', rotas);
 
